b4936bb620349ed34a5722261683fbd586053b96
IcaroMatias <0001155469@senaimgaluno.com.br>
2026-02-09 11:19:06 -0300

    codigo novo

--- src/main/java/com/atividade/ecommerce_atv2/dao/ProdutosDAO.java
+++ src/main/java/com/atividade/ecommerce_atv2/dao/ProdutosDAO.java
@@ -1,4 +1,56 @@
 package com.atividade.ecommerce_atv2.dao;
 
+import com.atividade.ecommerce_atv2.model.Produto;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProdutosDAO {
+    private Connection connection;
+
+    public ProdutosDAO {
+        this.connection = new ConnectionFactory().getConnection();
+    }
+
+    public void salvar(Produto produto) throws SQLException {
+        String sql = "INSERT INTO produtos (nome, preco) VALUES (?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1,produto.getNome());
+            stmt.setDouble(2, produto.getPreco());
+        }
+    }
+    public List<Produto> listarTodos() throws SQLException {
+        List<Produto> produtos = new ArrayList<>();
+        String sql = "SELECT * FROM produtos";
+        try (PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                Produto p = new Produto();
+                p.setId(rs.getInt("id"));
+                p.setNome(rs.getString("nome"));
+                p.setPreco(rs.getDouble("preco"));
+                produtos.add(p);
+            }
+        }
+        return produtos;
+    }
+
+    public void atualizar(Produto produto) throws SQLException {
+        String sql = "UPDATE produtos SET nome = ?, preco = ? WHERE id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, produto.getNome());
+            stmt.setDouble(2, produto.getPreco());
+            stmt.setInt(3, produto.getId());
+            stmt.execute();
+        }
+    }
+
+    public void deletar(int id) throws SQLException {
+        String sql = "DELETE FROM produtos WHERE  id = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            stmt.execute();
+        }
+    }
 }
